61477f35d6995e5125b993cd6b6f6f6907eb0393
Jack Dougherty <jack.dougherty@trincoll.edu>
2016-02-17 19:43:40 -0500

    initial upload

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # leaflet-map-panes
 Leaflet JS custom map panes tutorial with Connecticut town boundaries
+
+## Demo
+https://jackdougherty.github.io/leaflet-map-panes/
+
+## Requires
+- Leaflet 1.x
+
+## Based on Leaflet custom map panes tutorial
+- http://leafletjs.com/examples/map-panes.html
+
+## To Do
+- see if additional CT town layer names can be created and hosted in CartoDB
+- rebuild geojson data file for pop density
+- change from click to hover, and add legend to display colors and hover data
---
+++ script.js
@@ -0,0 +1,74 @@
+// Map panes requires Leaflet 1.x
+// See Leaflet tutorial http://leafletjs.com/examples/map-panes.html
+// template by http://github.com/jackdougherty/leaflet-map/
+
+// set up the map center and zoom level
+var map = L.map('map', {
+  center: [41.5, -72.7], // [41.5, -72.7] for Connecticut; [41.76, -72.67] for Hartford county or city
+  zoom: 9, // zoom 9 for Connecticut; 10 for Hartford county, 12 for Hartford city
+  zoomControl: false // add later to reposition
+});
+
+// customize link to view source code; add your own GitHub repository
+map.attributionControl
+.setPrefix('View <a href="http://github.com/jackdougherty/leaflet-map-panes">code on GitHub</a>, created with <a href="http://leafletjs.com" title="A JS library for interactive maps">Leaflet</a>');
+
+// This pane is above markers but below popups
+// see default pane z-index levels: https://github.com/Leaflet/Leaflet/blob/master/dist/leaflet.css#L73
+map.createPane('labels');
+map.getPane('labels').style.zIndex = 650;
+
+// Layers in this pane are non-interactive and do not obscure mouse/touch events
+map.getPane('labels').style.pointerEvents = 'none';
+
+// optional: add legend to toggle any baselayers and/or overlays
+// global variable with (null, null) allows indiv layers to be added inside functions below
+var controlLayers = L.control.layers( null, null, {
+  position: "bottomright", // suggested: bottomright for CT (in Long Island Sound); topleft for Hartford region
+  collapsed: false // false = open by default
+}).addTo(map);
+
+// optional: reposition zoom control other than default topleft
+L.control.zoom({position: "topright"}).addTo(map);
+
+/* BASELAYERS */
+var lightNoLabels = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+}).addTo(map);
+
+// see use of "pane" in this only_labels layer
+var lightOnlyLabels = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
+    pane: 'labels'
+}).addTo(map);
+
+/* POLYGON OVERLAY */
+// load polygon geojson, using data to define fillColor, from local directory
+// *TO DO* rebuild file for pop density
+// *TO DO* change from click to hover, and add legend to display colors and hover data
+$.getJSON("polygons.geojson", function (data) {   // insert pathname to your local directory file
+  var geoJsonLayer = L.geoJson(data, {
+    style: function (feature) {
+      var fillColor,
+        population = feature.properties.Pop2010;
+      if (population > 100000) fillColor = "#006837";
+      else if (population > 50000) fillColor ="#31a354";
+      else if (population > 15000) fillColor ="#78c679";
+      else if (population > 5000) fillColor ="#c2e699";
+      else if (population > 0) fillColor ="#ffffcc";
+      else fillColor = "#f7f7f7"; // no data
+      return {
+        'color': 'red',
+        'weight': 2,
+        'fillColor': fillColor, // sorts by method above
+        'fillOpacity': 0.8
+      }
+    },
+    onEachFeature: function( feature, layer) {
+      var popupText = "<b>" + feature.properties.Town + "</b>"   // replace labels with those from your own geojson
+         + "<br>Population 2010: " + "<br>" + feature.properties.Pop2010;
+      layer.bindPopup(popupText);
+    }
+  }).addTo(map);
+  controlLayers.addOverlay(geoJsonLayer, 'CT Pop 2010');  // insert your 'Title' to add to legend
+});
